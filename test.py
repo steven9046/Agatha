5f284e25fc574fe29b44f9c4f250c1b1f65ae4b0
chsun <451812667@qq.com>
2024-11-09 21:09:29 +0800

    1. Add a logger to recoder logs. 2. Add a keyboard monitoring thread to monitor if user press Q to    terminate the script
    Signed-off-by: chsun <451812667@qq.com>

--- test.py
+++ test.py
@@ -2,26 +2,55 @@ import pyautogui
 import cv2
 import os
 import numpy as np
-# from loguru import logger
+# from loguru import snap_logger
 import time
 import threading
 import json
 # from cnocr import CnOcr
 from paddleocr import PaddleOCR
 import logging
-
-logger = logging.getLogger('ppocr')
-logger.setLevel(logging.WARNING)
+import msvcrt
 
 
-regions = [(600, 1100, 900, 1258)]
+run_flag = True
+
+ppocr_logger = logging.getLogger('ppocr')
+ppocr_logger.setLevel(logging.WARNING)
+
+
+# 配置日志
+# 获取Logger实例
+snap_logger = logging.getLogger(__name__)
+snap_logger.setLevel(logging.DEBUG)  # 设置Logger的级别为DEBUG
+
+# 创建一个文件处理器，并设置级别为DEBUG
+current_timestamp = time.time()
+# print("当前时间戳:", current_timestamp)
+# 将时间戳转换为本地时间
+local_time = time.localtime(current_timestamp)
+# print(local_time)
+# print("本地时间:", time.strftime("%Y-%m-%d-%H-%M-%S", local_time))
+file_handler = logging.FileHandler('./snap_log/' + time.strftime("%Y-%m-%d-%H-%M-%S", local_time) + ".log")
+file_handler.setLevel(logging.DEBUG)
+# 创建一个日志格式
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+# 设置文件处理器的日志格式
+file_handler.setFormatter(formatter)
+# 将文件处理器添加到Logger实例
+snap_logger.addHandler(file_handler)
+# 记录不同级别的日志
+# snap_logger.debug('This is a debug message')
+# snap_logger.info('This is an info message')
+# snap_logger.warning('This is a warning message')
+# snap_logger.error('This is an error message')
+# snap_logger.critical('This is a critical message')
 
 class Button():
     def __init__(self, name):
         self.name = name
-        print("name : ", name)
+        # print("name : ", name)
         self.image_path = os.path.join(os.getcwd(), 'marvel_script', 'resource', name + '.png')
-        print("image_path: ", self.image_path)
+        # print("image_path: ", self.image_path)
         self.img = cv2.imread(self.image_path)
         # self.location = pyautogui.locateCenterOnScreen(self.image_path, grayscale=True, confidence=0.5)
         # if self.location:
@@ -45,7 +74,10 @@ class Button():
  
 
 class SnapWin():
-    def __init__(self, win):
+    def __init__(self, win, logger_):
+        
+        self.logger = logger_
+        self.logger.debug("Creating a SnapWin instance...")
         self.win = win  # 实例属性
         self.buttons = []
         self.button_names = ["start_button", "snap", "giveup_button", "get_reward", "next", "confirm_retreat"]
@@ -68,6 +100,7 @@ class SnapWin():
 
         
     def loadButtons(self):
+        self.logger.debug("Loading buttons ...")
         for button_name in self.button_names:
             tmp_button = Button(button_name)
             self.buttons.append(tmp_button)
@@ -94,7 +127,7 @@ class SnapWin():
                 mask = (labels_im == i).astype(np.uint8) * 255
                 ret, thresh_trunc = cv2.threshold(mask, 0, 255, cv2.THRESH_BINARY) 
                 result = self.checkText(thresh_trunc)
-                print("Cube: ", result)
+                # print("Cube: ", result)
                 if result[0]:
                     self.cube = result[0][0][1][0]
                 else:
@@ -102,7 +135,7 @@ class SnapWin():
         # res = self.checkText(app_screenshot[-160:,400:500]) # 能量在中间 (1258, 900)
         # print(res)
         # self.current_energy = res[0][0][1][0]
-        print("Cube: ", self.cube)        
+        # print("Cube: ", self.cube)        
 
 
     ## 检测能量的优先级比较高，因为能量会变化, 所以应该
@@ -113,7 +146,7 @@ class SnapWin():
     def checkEnergy(self, app_screenshot):
         # 返回的是一个 list
         res = self.checkText(app_screenshot[-160:,400:500])[0] # 能量在中间 (1258, 900)
-        print(res)
+        # print(res)
         ## res 得有内容，并且是检测出来了 [回合结束，1/6] 这种文字，才会去改当前回合数
         if res and len(res) == 2:
             ## 只有检测出变化了才会去改, 这样可以保持只修改一次回合数和能量数
@@ -121,7 +154,7 @@ class SnapWin():
             if self.current_energy != res[0][1][0]:
                 if res[0][1][0] in self.energy:
                     self.current_energy = res[0][1][0]
-                    print("Updating Energy: ", self.current_energy)        
+                    # print("Updating Energy: ", self.current_energy)        
 
 
     def checkRound(self, app_screenshot):
@@ -144,6 +177,7 @@ class SnapWin():
                 self.current_round = res[1][1][0]
                 self.is_7_round = True
                 # print("回合中... ", self.round_idx)
+            self.logger.debug(f"回合中... {self.round_idx}")
             # elif res[0][1][0] != "回合结束":
             #     print("放置卡牌中...")
             #     self.round_idx += 1
@@ -164,7 +198,7 @@ class SnapWin():
             self.snapped = 1
         if self.round_idx > 4:
             if self.is_7_round:
-                print("7 回合啦，快跑！")
+                self.logger.warning("7 回合啦，快跑！")
                 self.retreat()
                 return
             # if self.snapped:
@@ -175,16 +209,16 @@ class SnapWin():
     def checkFinish(self, img):
         if self.buttons[3].checkExisting((600, 1100, 900, 1258)) :
             pyautogui.click(self.buttons[3].location)
-            print("click reward : ", self.buttons[3].location)
+            self.logger.debug(f"click reward : {self.buttons[3].location}")
             time.sleep(1)
         if self.buttons[4].checkExisting((600, 1100, 900, 1258)):
             pyautogui.click(self.buttons[4].location)
-            print("click next round: ", self.buttons[4].location) 
+            self.logger.debug(f"click next round: {self.buttons[4].location}", ) 
 
     def checkStart(self, img):
         if self.buttons[0].checkExisting((300, 940, 300, 200)) :
-            print("click start : ", self.buttons[0].location)
-            print("total round: ", self.total_round)
+            self.logger.debug(f"click start : {self.buttons[0].location}")
+            self.logger.debug(f"total round: {self.total_round}")
             pyautogui.click(self.buttons[0].location)
             time.sleep(1)
             self.round_idx = 0
@@ -197,6 +231,7 @@ def get_screen():
     return image_np
 
 def resetWindow(win):
+    snap_logger.debug("Resize sanp window to 900x1258 and mover to 0,0")
     win.resizeTo(900, 1258)
     win.moveTo(0, 0)
 
@@ -207,50 +242,21 @@ def resetWindow(win):
 # def winStatusMonitoring(win):
 #     while True:
         
-
-if __name__ == "__main__":
-    print("hello, marvel!")
-    screen_width, screen_height = pyautogui.size()
-    print(f"Screen size: {screen_width}x{screen_height}")
-    print("Current working directory:", os.getcwd())
-
-    snap_window_name = "SnapCN"
-    # allWindows = pyautogui.getAllWindows()
-    # print(allWindows)
-    allTitles = pyautogui.getAllTitles() # SnapCN
-    if snap_window_name not in allTitles:
-        logger.error("SnapCN is not activated!!!")
-    else:
-        sanp_win = pyautogui.getWindowsWithTitle(snap_window_name)[0]
-        resetWindow(sanp_win)
-
-        sanp_win.activate()
-        
-        time.sleep(2) # 等待窗口切换到前台
-        snapWin = SnapWin(sanp_win)
-        snapWin.loadButtons()
-
-    # thread = threading.Thread(target=thread_function, args=("1",))
-
-
-
-    # app_screenshot = cv2.imread(os.path.join(os.getcwd(), 'marvel_script', 'resource', 'round_2.png'))
-
-    # print("app_screenshot: ", app_screenshot.shape) #  (1258, 900, 3)
-
-
-    # cv2.imshow('OpenCV Image', app_screenshot[-140:,400:500]) # [-150:,-200:] 这个坐标大概是结束回合的按钮
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
-
-
-    # snapWin.checkRound(app_screenshot)
-    # snapWin.checkEnergy(app_screenshot)
-    # snapWin.checkCube(app_screenshot)
-
-    # snapped = 0
+def terminate_loop():
+    global run_flag
     while True:
+        if msvcrt.kbhit():  # 检查键盘是否有按键按下
+            key = msvcrt.getch()  # 读取按键
+            if key.decode().upper() == 'Q':
+                snap_logger.warning("Pressing Q to terminate!")
+                run_flag = False
+                print("run_flag :", run_flag)
+                break
 
+def main_loop():
+    global run_flag
+    while run_flag:
+        # snap_logger.warning("looping ...")
         app_screenshot = pyautogui.screenshot(region=(sanp_win.left, sanp_win.top, sanp_win.width, sanp_win.height))
         app_screenshot = np.array(app_screenshot)
         app_screenshot = cv2.cvtColor(app_screenshot, cv2.COLOR_RGB2BGR)
@@ -264,10 +270,56 @@ if __name__ == "__main__":
         snapWin.checkStart(app_screenshot)
         time.sleep(1)
 
-    currentMouseX, currentMouseY = pyautogui.position()
-    # region=(600, 1100, 900, 1258)
-    # 打印鼠标的位置
-    print(f"当前鼠标位置：X = {currentMouseX}, Y = {currentMouseY}") # 799, Y = 1167
+
+if __name__ == "__main__":
+    snap_logger.info('Hello Snap!')
+    screen_width, screen_height = pyautogui.size()
+    snap_logger.debug(f"Screen size: {screen_width}x{screen_height}")
+    snap_logger.debug(f"Current working directory: " + os.getcwd())
+
+    snap_window_name = "SnapCN"
+    # allWindows = pyautogui.getAllWindows()
+    allTitles = pyautogui.getAllTitles() # SnapCN
+    if snap_window_name not in allTitles:
+        snap_logger.error("SnapCN is not activated!!!")
+    else:
+        sanp_win = pyautogui.getWindowsWithTitle(snap_window_name)[0]
+        resetWindow(sanp_win)
+
+        snap_logger.debug("Activate snap window.")
+        sanp_win.activate()
+        
+        time.sleep(2) # 等待窗口切换到前台
+        snapWin = SnapWin(sanp_win, snap_logger)
+        snapWin.loadButtons()
+
+    # thread = threading.Thread(target=thread_function, args=("1",))
+
+    # app_screenshot = cv2.imread(os.path.join(os.getcwd(), 'marvel_script', 'resource', 'round_2.png'))
+    # print("app_screenshot: ", app_screenshot.shape) #  (1258, 900, 3)
+    # cv2.imshow('OpenCV Image', app_screenshot[-140:,400:500]) # [-150:,-200:] 这个坐标大概是结束回合的按钮
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+
+    # snapWin.checkRound(app_screenshot)
+    # snapWin.checkEnergy(app_screenshot)
+    # snapWin.checkCube(app_screenshot)
+
+
+    terminate_loop_thread = threading.Thread(target=terminate_loop)
+    terminate_loop_thread.start()
+
+
+    main_loop_thread = threading.Thread(target=main_loop)
+    main_loop_thread.start()
+    
+    terminate_loop_thread.join()
+    main_loop_thread.join()
+
+    # currentMouseX, currentMouseY = pyautogui.position()
+    # # region=(600, 1100, 900, 1258)
+    # # 打印鼠标的位置
+    # print(f"当前鼠标位置：X = {currentMouseX}, Y = {currentMouseY}") # 799, Y = 1167
 
 
 
